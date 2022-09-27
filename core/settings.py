8ff36cff557af5f8f609e9a496026a78e20f4e02
Sagar Neupane <cyber.crime252@gmail.com>
2022-09-27 09:13:04 +0545

    Use of Google Translation API

--- core/settings.py
+++ core/settings.py
@@ -115,3 +115,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_URL = 'static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = [STATIC_DIR]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
